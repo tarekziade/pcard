952d1668fbdccdddb5014510be2c5cec7e8c52c9
Tarek Ziade <tarek@ziade.org>
2012-03-11 16:36:20 -0700

    more metadata

--- setup.py
+++ setup.py
@@ -7,8 +7,12 @@ requires = ['bottle']
 
 setup(name='pcard',
       version='0.1',
+      author="Tarek Ziade",
+      description="Generates a Password card",
+      author_email="tarek@ziade.org",
       packages=find_packages(),
       include_package_data=True,
+      licence='MIT',
       zip_safe=False,
       install_requires=requires,
       entry_points="""\
